24f661045ace73fbed5f7182601b5113fa12f4c3
Tejas Shah <tejashah88@gmail.com>
2019-01-04 15:56:10 -0800

    initial support for parsing agreements

---
+++ tools/old_site/parse_agreements.py
@@ -0,0 +1,81 @@
+import re
+from pprint import PrettyPrinter
+
+ezprint = PrettyPrinter(indent=4).pprint
+
+import numpy as np
+
+from .scrape_content import scrape_articulation_by_major
+
+from contexttimer import Timer
+
+def count_chars_for_lines(raw_txt):
+    return [len(line) for line in raw_txt.splitlines()]
+
+def split_agreement_by_blocks(raw_txt):
+    lines = raw_txt.splitlines()
+    num_dashes = [line.count('-') for line in lines]
+    mean_dashes = np.mean(num_dashes)
+
+    blocks = []
+    buffer = ""
+    for line in lines:
+        if line.count('-') >= mean_dashes:
+            blocks += [buffer]
+            buffer = ""
+        else:
+            buffer += line + '\n'
+
+    return blocks
+
+# since each valid block has an equal number of bars and newlines, we can split the block with both delimiters and
+def split_block_by_side(block):
+    block_parts = re.split('[|\n]', block)
+    print(block_parts)
+    origin_block = '\n'.join(block_parts[::2])
+    dest_block = '\n'.join(block_parts[1::2])
+    print(">>> ORIGIN BLOCK")
+    print(origin_block)
+    print(">>> END ORIGIN BLOCK")
+    print()
+    print(">>> DEST BLOCK")
+    print(dest_block)
+    print(">>> END DEST BLOCK")
+    print()
+    return [origin_block, dest_block]
+
+def split_all_course_blocks_by_side(blocks):
+    finished_blocks = []
+    for block in blocks:
+        count_newlines = sum(ch == '\n' for ch in block)
+        count_bars = sum(ch == '|' for ch in block)
+        finished_blocks += [split_block_by_side(block) if count_bars == count_newlines else block]
+
+    return finished_blocks
+
+def append_missing_spaces(raw_txt):
+    lines = raw_txt.splitlines()
+    max_len = max([len(line) for line in lines])
+    fixed_lines = [line.ljust(max_len) for line in lines]
+    return '\n'.join(fixed_lines)
+
+
+def parse_major_agreement():
+    # txt = scrape_articulation_by_major('DIABLO', '16-17', 'UCB', 'EECS')
+    # txt = scrape_articulation_by_major('DIABLO', '16-17', 'CSUC', 'ENGMT')
+    txt = scrape_articulation_by_major('SDSU', '16-17', 'CSULA', 'CE')
+    from time import time
+    start = time()
+
+    space_filled = append_missing_spaces(txt)
+    blocks = split_agreement_by_blocks(space_filled)
+    # print(count_chars_for_lines(space_filled))
+    [ezprint(block) for block in split_all_course_blocks_by_side(blocks)]
+    # split_all_course_blocks_by_side(blocks)
+
+    end = time()
+
+    print((end - start) * 1000, 'ms')
+
+if __name__ == "__main__":
+    parse_major_agreement()
