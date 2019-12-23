e0bc647b34629b786326bcbd7fa1592fd25de6f1
Tejas Shah <tejashah88@gmail.com>
2019-12-22 17:57:26 -0800

    added home page w/ tiles

---
+++ frontend/src/components/HomeTile.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card
+    class="mx-auto"
+    :max-width="maxWidth"
+    link
+    :to="route">
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="img">
+      <v-card-title>{{ title }}</v-card-title>
+    </v-img>
+
+    <v-card-text class="text--primary">
+      <div>{{ description }}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn text>Go</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeTile',
+  props: {
+    maxWidth: String,
+    title: String,
+    img: String,
+    description: String,
+    route: String,
+  },
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,65 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <v-col
+      cols="auto"
+      lg="10"
+      xl="6"
+      offset="0"
+      offset-lg="2"
+      offset-xl="3">
+      <v-row>
+        <v-col>
+          <HomeTile
+            title="Check transfer requirements"
+            img="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            description="I know my college and major, but I don't know my requirements needed to transfer..."
+            route="transfer-reqs"
+            max-width=400>
+          </HomeTile>
+        </v-col>
+
+        <v-col>
+          <HomeTile
+            title="Explore different majors"
+            img="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            description="I know which college I want to attend, but I'm not sure which major is right for me..."
+            route="explore-majors"
+            max-width=400>
+          </HomeTile>
+        </v-col>
+
+        <v-col>
+          <HomeTile
+            title="Explore different universities"
+            img="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            description="I know which major that I want to study, but I'm not sure which college is right for me..."
+            route="explore-universities"
+            max-width=400>
+          </HomeTile>
+        </v-col>
+
+        <v-col>
+          <HomeTile
+            title="About the project"
+            img="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            description="Check out what this website is all about, and what you can do with it!"
+            route="about"
+            max-width=400>
+          </HomeTile>
+        </v-col>
+      </v-row>
+    </v-col>
+  </div>
 </template>
 
 <script>
+
+import HomeTile from '@/components/HomeTile';
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeTile
+  }
 };
 </script>
