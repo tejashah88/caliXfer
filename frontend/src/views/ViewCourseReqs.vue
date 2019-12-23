dd503193c89332aca9805222f62179b5ce9c6026
Tejas Shah <tejashah88@gmail.com>
2019-12-22 17:57:01 -0800

    course reqs => transfer reqs

--- frontend/src/views/ViewCourseReqs.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>View Course Requirements</h1>
-</template>
-
-<script>
-export default {
-  name: 'ViewCourseReqs'
-};
-</script>
---
+++ frontend/src/views/ViewTransferReqs.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>View Transfer Requirements</h1>
+</template>
+
+<script>
+export default {
+  name: 'ViewTransferReqs'
+};
+</script>
