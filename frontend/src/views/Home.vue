1fe6c9a6db62696ed9d240a345ffa6293b2dd48f
Tejas Shah <tejashah88@gmail.com>
2019-12-23 16:43:07 -0800

    changed pics for "home" page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -11,7 +11,7 @@
         <v-col>
           <HomeTile
             title="Check transfer requirements"
-            img="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            img="https://www.midlandstech.edu/sites/default/files/bc_8182.jpg"
             description="I know my college and major, but I don't know my requirements needed to transfer..."
             route="transfer-reqs"
             max-width=400>
@@ -21,7 +21,7 @@
         <v-col>
           <HomeTile
             title="Explore different majors"
-            img="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            img="https://zen.collegevine.com/wp-content/uploads/2016/07/college-majors-istock_000017855571xlarge-1280x640.jpg"
             description="I know which college I want to attend, but I'm not sure which major is right for me..."
             route="explore-majors"
             max-width=400>
@@ -31,7 +31,7 @@
         <v-col>
           <HomeTile
             title="Explore different universities"
-            img="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            img="https://www.southwestjournal.com/wp-content/uploads/2017/05/Walsh0518web.jpg"
             description="I know which major that I want to study, but I'm not sure which college is right for me..."
             route="explore-universities"
             max-width=400>
@@ -41,7 +41,7 @@
         <v-col>
           <HomeTile
             title="Explore majors and universities"
-            img="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            img="https://www.everystudent.com/3/share2019/universe2.jpg"
             description="I don't know which college to attend and which major to study..."
             route="about"
             max-width=400>
