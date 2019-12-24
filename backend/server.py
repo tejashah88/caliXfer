ed9370a4ae7e8d454aaa9e9de6f753f7f6db2cff
Tejas Shah <tejashah88@gmail.com>
2019-12-23 16:43:50 -0800

    temporarily putting flask backend code (still using old custom API)

--- backend/server.py
+++ backend/server.py
@@ -1,32 +1,40 @@
-import hug
-import bjoern
-import pymongo
+from flask import Flask, jsonify
+
+# import bjoern
+# import pymongo
+
+# apparently trying to import modules from the parent directory is near impossible without this hack
+import sys
+sys.path.append("..")
 
 from tools.old_site.scrape_possibilities import *
 from tools.old_site.scrape_content import *
 
-@hug.get('/options/origins')
+app = Flask(__name__)
+
+@app.route('/get-origin-schools')
 def get_origin_schools():
-    return scrape_origin_schools()
+    return jsonify(scrape_origin_schools())
 
-@hug.get('/options/year-range')
+@app.route('/get-year-ranges')
 def get_year_ranges(origin):
-    return scrape_year_ranges(origin)
+    return jsonify(scrape_year_ranges(origin))
 
-@hug.get('/options/destinations')
+@app.route('/get-destination-schools')
 def get_dest_schools(origin, years):
-    return scrape_dest_schools(origin, years)
+    return jsonify(scrape_dest_schools(origin, years))
 
-@hug.get('/options')
+@app.route('/get-possible-majors')
 def get_possible_majors(origin, years, dest):
-    return scrape_dest_majors(origin, years, dest)
+    return jsonify(scrape_dest_majors(origin, years, dest))
 
-@hug.get('/agreements/major', output=hug.output_format.text)
+@app.route('/get-agreement')
 def get_articulation_agreement_by_major(origin, years, dest, major):
     return scrape_articulation_by_major(origin, years, dest, major)
 
 if __name__ == '__main__':
-    IP = '127.0.0.1'
-    PORT = 8080
-    print(f'Running server on {IP}:{PORT}')
-    bjoern.run(__hug_wsgi__, IP, PORT)
+    app.run(debug=True)
+    # IP = '127.0.0.1'
+    # PORT = 8080
+    # print(f'Running server on {IP}:{PORT}')
+    # bjoern.run(app.wsgi_app, IP, PORT)
