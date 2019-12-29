05464a1ab8cfc10f75d5d5b86ba0b9f2374311c9
Tejas Shah <tejashah88@gmail.com>
2019-12-28 16:50:19 -0800

    added fetching of years

--- frontend/src/pseudo-assist-api.js
+++ frontend/src/pseudo-assist-api.js
@@ -1,19 +1,39 @@
 const axios = require('axios');
-const memoize = require("memoizee");
+const memoize = require('memoizee');
 
 const api = axios.create({
   baseURL: 'http://localhost:5000'
 });
 
 const AssistAPI = {
+  fetchYears() {
+    return api.get('/academic-years')
+      .then(res => res.data)
+      .then(years => {
+        return years.map(year => {
+          return { 'id': year['id'], 'fall-year': `${year['fall-year']} - ${year['fall-year']+1}` };
+        });
+      });
+  },
   fetchSourceInstitutions() {
-    return api.get('/fetch-src-institutions').then(res => res.data);
+    return api.get('/origin-institutions').then(res => res.data);
   },
   fetchTargetInstitutions(srcSchoolId) {
-    return api.get('/fetch-dst-institutions', { params: { 'src_school_id': srcSchoolId }}).then(res => res.data);
+    return api.get('/dest-institutions', {
+      params: {
+        'src_school_id': srcSchoolId
+      }
+    }).then(res => res.data);
   },
   fetchMajor(srcSchoolId, dstSchoolId, yearId) {
-    return api.get('/fetch-agreements-by-category', { params: { 'src_school_id': srcSchoolId, 'dst_school_id': dstSchoolId, 'year_id': yearId, 'category_code': 'major' }}).then(res => res.data.reports);
+    return api.get('/fetch-agreements-by-category', {
+      params: {
+        'src_school_id': srcSchoolId,
+        'dst_school_id': dstSchoolId,
+        'year_id': yearId,
+        'category_code': 'major'
+      }
+    }).then(res => res.data.reports);
   }
 };
 
