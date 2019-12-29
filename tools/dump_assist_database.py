51e06c0c3728412b3da54c8321dfd11ed67c7a7b
Tejas Shah <tejashah88@gmail.com>
2019-12-28 23:31:42 -0800

    [tools]: added file for (partially?) dumping ASSIST DB

---
+++ tools/dump_assist_database.py
@@ -0,0 +1,9 @@
+from assist_api import AssistAPI
+
+if __name__ == "__main__":
+    api = AssistAPI(dump_json=True)
+    years = api.fetch_academic_years()
+    src_schools = api.fetch_institutions()
+    for src_school in src_schools:
+        print(src_school)
+
